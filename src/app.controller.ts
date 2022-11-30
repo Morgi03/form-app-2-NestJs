ac53320c271ae9a8de16c554480784a4be1f0193
Morgován Nimród <morgovannimrod22@gmail.com>
2022-11-30 12:38:41 +0100

    body object

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Get, Controller, Render, Post, Body } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
   @Render('form')
+  getHello(): object {
+    return {};
+  }
   root() {
     return {};
+  }
+  @Post()
+  handleColor(@Body() body: object): string {
+    return 'OK ' + Object.values(body);
   }
 }
